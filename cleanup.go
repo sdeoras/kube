6d1f403491c19d49f51e4561ab92bfb01eecf045
Saurabh Deoras <sdeoras@gmail.com>
2018-04-30 18:23:06 -0700

    updated cleanup function

--- cleanup.go
+++ cleanup.go
@@ -2,33 +2,12 @@ package kube
 
 import (
 	"context"
-	"sync"
 )
 
-// Cleanup sends delete commands to all coders and provides a channel to wait on
-func Cleanup(ctx context.Context, coders ...Coder) <-chan struct{} {
-	// shutdown all if error occurs
-	var wg sync.WaitGroup
-	for _, coder := range coders {
-		coder := coder
-		go func(coder Coder) {
-			wg.Add(1)
-
-			delete := coder.Delete(ctx)
-			select {
-			case <-coder.Error():
-			case <-delete.Done(): // if no error, block till delete
-			}
-
-			wg.Done()
-		}(coder)
-	}
-
-	cleanup := make(chan struct{})
-	go func() {
-		wg.Wait()
-		cleanup <- struct{}{}
-	}()
-
-	return cleanup
+// Cleanup sends delete commands to all coders
+func Cleanup(coders ...Coder) context.Context {
+	trigger, startFunc := context.WithCancel(context.Background())
+	trigger, _ = Delete(trigger, Async, coders...)
+	go startFunc()
+	return trigger
 }
