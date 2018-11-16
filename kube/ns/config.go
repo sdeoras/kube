46ec6f18ba30c2b1e621c5ea10a5866eeb1780e0
Saurabh Deoras <saurabh@portworx.com>
2018-11-16 05:12:30 -0800

    namespace kind and apiver added

--- kube/ns/config.go
+++ kube/ns/config.go
@@ -18,6 +18,8 @@ func (conf *Config) Init(key string) *Config {
 	conf.Namespace = new(v1.Namespace)
 	conf.Namespace.Namespace = key
 	conf.Namespace.Name = strings.ToLower(key) + "-name"
+	conf.Namespace.Kind = Kind
+	conf.Namespace.APIVersion = ApiVersion
 	conf.key = key
 	return conf
 }
--- kube/ns/const.go
+++ kube/ns/const.go
@@ -4,4 +4,6 @@ const (
 	PackageName       = "kube/ns"
 	DefaultConfigDir  = ".config/" + PackageName
 	DefaultConfigFile = "config.json"
+	Kind              = "Namespace"
+	ApiVersion        = "v1"
 )
